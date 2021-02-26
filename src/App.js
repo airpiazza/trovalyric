1e803a725167cf5c0e62ea504981adce311bd977
airpiazza <nick@piazzaonline.com>
2021-02-26 12:22:49 -0800

    added description using jsx

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <Header />;
+      <Header />
+      <Description />
     </div>
   );
 }
 
 function Header(){
   return React.createElement('h1', null, 'trovalyric');
+}
+
+function Description(){
+  return (
+    <p>
+      Straight up lyrics.      
+    </p>
+  )
 }
 
 export default App;
